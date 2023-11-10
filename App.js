e0b5d0b6e912e2f244992816408f3ed838b2a4ce
TisSTUD <laetitiazidane18@gmail.com>
2023-11-10 19:53:34 +0100

    updating versions

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { View } from 'react-native';
 import Home from './src/screens/home';
 import Splash from './src/screens/splash';
 
 const Stack = createNativeStackNavigator();
 const App = ()=>{
   return (
-          <NavigationContainer>
-            <Stack.Navigator screenOptions={{headerShown:false}} >
-              
-              <Stack.Screen name="Splash" component={Splash} />
-              <Stack.Screen name="Home" component={Home} />
-              {/* <Stack.Screen name="Login" component={Login} /> */}
-            </Stack.Navigator>
-          </NavigationContainer>
+    <View style={{ flex: 1,}}>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerShown:false}} >
+          <Stack.Screen name="Splash" component={Splash} />
+            <Stack.Screen name="Home" component={Home} />
+            {/* <Stack.Screen name="Login" component={Login} /> */}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </View>
   );
 }
 
